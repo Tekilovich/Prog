cff79c8153d4d9ac1a1c5ead28d0af07748e26ce
Michael <mail>
2018-01-19 16:35:01 +0200

    lesson1

--- lesson1/task2/TextContainer.java
+++ lesson1/task2/TextContainer.java
@@ -1,15 +1,24 @@
 package lesson1.task2;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-@SaveTo(path = "d:\\java\\file.txt")
+@SaveTo(path = "lesson1/task2/file.txt")
 public class TextContainer {
-    String text = "Someone text";
+    private String text = "Someone text";
 
     @Saver
     public void save(String path) {
-        try (FileWriter writer = new FileWriter(path)) {
+        File file = new File(path);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try (FileWriter writer = new FileWriter(file)) {
             writer.write(text);
 
         } catch (IOException e) {
